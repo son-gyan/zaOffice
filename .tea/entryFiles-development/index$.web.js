2453774f0ca498419bab4b6f129a6542e9437cbd
song.yan <song.yan@diveo.com.cn>
2020-11-13 15:12:51 +0800

    ys

---
+++ .tea/entryFiles-development/index$.web.js
@@ -0,0 +1,8 @@
+require('@alipay/appx-compiler/lib/sjsEnvInit');
+require('./config$');
+
+require('../../pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/login/login?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/approval/approval?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/message/message?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/about/about?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
